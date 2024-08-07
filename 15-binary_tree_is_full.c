b59d4e0f624bb6286b86d14dec844232eeb90754
root <9548holbertonstudents.com>
2024-08-07 09:38:04 +0200

    binary_tree_is_full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -24,17 +24,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
  *
  * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 0 if tree is not NULL, otherwise 1.
+ * Return: 1 if tree is not NULL, otherwise 0.
  */
 
 int is_full_recursive(const binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 		return (1);
 	{
 	if ((tree->left != NULL && tree->right == NULL) ||
-		(tree->left == NULL && tree->right != NULL) ||
-		    
+		(tree->left == NULL && tree->right != NULL))
 	return (0);
 	}
 	return (is_full_recursive(tree->left) && is_full_recursive(tree->right));
